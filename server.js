60650398658de0b2923474d700065670a038725a
willhogan370 <wfhogan@umass.edu>
2022-04-20 10:18:33 -0400

    Endpoints updated

--- server.js
+++ server.js
@@ -10,20 +10,22 @@ app.use('/public', express.static("public"));
 ​
 const MESSAGE_FILE = 'messages.json'
 const CLASS_FILE = 'classes.json'
-const USER_FILE = 'users.json'
+const USER_FILE = 'user_records.json'
 
 //Dashboard
 
-let user_records = [{ name: 'Joe', instructor: 'False', user_id: '1', inClasses: '[1, 2]'},
-{ name: 'Jeff', instructor: "False", id: '2', inClasses: '[1, 3]'}];
+let user_records = [{ name: 'Joe', instructor: 'False', user_id: '1', inClasses: [1, 2]},
+{ name: 'Jeff', instructor: "False", user_id: '2', inClasses: [1, 3]}];
 
-let class_records = [{ class: 'cs326', studentcount: '187', class_id: '1' },
-{ class: 'cs446', studentcount: '146', id: '2' }, { class: 'cs365', studentcount: '90', id: '3' },
+let class_records = [{ class: 'cs326', studentcount: '187', class_id: '1' , professor: 'Tim Richards'},
+{ class: 'cs446', studentcount: '146', class_id: '2' }, { class: 'cs365', studentcount: '90', id: '3' },
 { class: 'econ103', studentcount: '250', id: '4' }];
 
 let assignment_records = [{class_id: '3', day: '20', month: '3', year: '2022', assignment_name: 'homework 5', assignment_id: '7'}];
 ​
-//Unsure if needed, are we going to add classes or is that going to be static?
+
+/*
+WILL BE USED ONCE DATA BASE IS BEING USED
 async function reloadClasses() {
     try {
         const data = await readFile(CLASS_FILE, { encoding: 'utf8' });
@@ -42,17 +44,18 @@ async function saveClasses() {
         class_records = [];
     }
 }
+*/
 
-
-app.get('/user/dashboard', async (request, response) => {
+app.get('/dashboard', async (request, response) => {
     const options = request.query; 
-    const classesArr = options.inClasses;
+    const user = options.user;
+    const classesArr = user_records.filter(item => item.user_id === user)[0].inClasses;
     let userClasses = class_records.filter(item => classesArr.includes(item.class_id));
     console.log(userClasses);
     response.send(userClasses);
 })
 
-app.get('/dashboard/class/dueToday', async (request, response) =>{
+app.get('/dueToday', async (request, response) =>{
     const options = request.query; 
     const class_id = options.id;
     const current = new Date();
@@ -67,14 +70,6 @@ app.get('/dashboard/class/dueToday', async (request, response) =>{
     response.send(returnVal); 
 });
 
-
-//WE could use get DATE from caldnera API instead, this would make our work a lot easier and it is
-//ultimately a design choice. 
-app.get('/dashboard/class/calendar/nextAssignment', async(request, response) => {
-    
-
-});
-​
 
 
 app.all('*', async (request, response) => {
