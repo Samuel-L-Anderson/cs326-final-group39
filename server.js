7f81fc0ce587c49e68654af7000df7c6c6e87314
willhogan370 <wfhogan@umass.edu>
2022-04-20 08:55:56 -0400

    Updating Server Endpoints

--- server.js
+++ server.js
@@ -14,12 +14,14 @@ const USER_FILE = 'users.json'
 
 //Dashboard
 
-let user_records = [{ name: 'Joe', instructor: 'False', id: '1', inClasses: '[1, 2]'},
+let user_records = [{ name: 'Joe', instructor: 'False', user_id: '1', inClasses: '[1, 2]'},
 { name: 'Jeff', instructor: "False", id: '2', inClasses: '[1, 3]'}];
 
-let class_records = [{ class: 'cs326', studentcount: '187', id: '1' },
+let class_records = [{ class: 'cs326', studentcount: '187', class_id: '1' },
 { class: 'cs446', studentcount: '146', id: '2' }, { class: 'cs365', studentcount: '90', id: '3' },
 { class: 'econ103', studentcount: '250', id: '4' }];
+
+let assignment_records = [{class_id: '3', day: '20', month: '3', year: '2022', assignment_name: 'homework 5', assignment_id: '7'}];
 ​
 //Unsure if needed, are we going to add classes or is that going to be static?
 async function reloadClasses() {
@@ -42,19 +44,34 @@ async function saveClasses() {
 }
 
 
-app.get('/dashboard', async (request, response) => {
+app.get('/user/dashboard', async (request, response) => {
     const options = request.query; 
-    const classesArr = options.user.inClasses;
-    let userClasses = class_records.filter(item => classesArr.includes(item.id));
+    const classesArr = options.inClasses;
+    let userClasses = class_records.filter(item => classesArr.includes(item.class_id));
     console.log(userClasses);
-    response.sent(userClasses);
+    response.send(userClasses);
 })
+
+app.get('/dashboard/class/dueToday', async (request, response) =>{
+    const options = request.query; 
+    const class_id = options.id;
+    const current = new Date();
+    let dueToday = assignment_records.filter(item => (
+        class_id === item.class_id &&
+        item.day === current.getDay() &&
+        item.month === current.getMonth() &&
+        item.year === current.getFullYear() 
+        ));
+    const returnVal = (dueToday.length > 0) ? dueToday[0].assignment_name : "N/A";
+    console.log(returnVal);
+    response.send(returnVal); 
+});
 
 
 //WE could use get DATE from caldnera API instead, this would make our work a lot easier and it is
 //ultimately a design choice. 
 app.get('/dashboard/class/calendar/nextAssignment', async(request, response) => {
-    const options = request.query; 
+    
 
 });
 ​
