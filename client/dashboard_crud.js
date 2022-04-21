e3c285e52cb1444698701ff25713b10d0e6b387c
willhogan370 <wfhogan@umass.edu>
2022-04-20 22:16:29 -0400

    done with dashboard backend functionality

--- client/dashboard_crud.js
+++ client/dashboard_crud.js
@@ -3,18 +3,26 @@ For this milestone user ID will be hardcoded, once the database is running then 
 user ID will be pulled from the user that is logged in 
 */
 export async function fetchDashboard(user) {
+    try {
     const response = await fetch(`/dashboard?user=${user}`, {
-        method: 'GET',
+        method: 'GET'
     });
     const data = await response.json();
     return data;
+    } catch(err) {
+        console.log(err);
+    }
 }
 
 export async function dueToday(clas) {
+    try {
     const response =  await fetch(`/dueToday?clas=${clas}`, {
-        method: 'GET',
+        method: 'GET'
     });
     const data = await response.json(); 
     return data; 
+    } catch(err) {
+        console.log(err);
+    }
 }
 
--- server.js
+++ server.js
@@ -1,56 +1,38 @@
 import express from 'express'; 
-import { readFile, writeFile } from 'fs/promises';
 import logger from 'morgan';
 
-​
+
+const app = express();
+const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
-app.use('/public', express.static("public"));
-​
+app.use('/client', express.static("client"));
+
 const MESSAGE_FILE = 'messages.json'
 const CLASS_FILE = 'classes.json'
 const USER_FILE = 'user_records.json'
 
 //Dashboard
 
-let user_records = [{ name: 'Joe', instructor: 'False', user_id: '1', inClasses: [1, 2]},
+let user_records = [{ name: 'Joe', instructor: 'False', user_id: '1', inClasses: [1, 2, 3]},
 { name: 'Jeff', instructor: "False", user_id: '2', inClasses: [1, 3]}];
 
 let class_records = [{ class: 'cs326', studentcount: '187', class_id: '1' , professor: 'Tim Richards'},
-{ class: 'cs446', studentcount: '146', class_id: '2' }, { class: 'cs365', studentcount: '90', id: '3' },
-{ class: 'econ103', studentcount: '250', id: '4' }];
+{ class: 'cs446', studentcount: '146', class_id: '2', professor: "Tom Brady"}, { class: 'cs365', studentcount: '90', id: '3', professor: "Bobby Oar"},
+{ class: 'econ103', studentcount: '250', id: '4', professor: "Mac Jones"}];
 
 let assignment_records = [{class_id: '3', day: '20', month: '3', year: '2022', assignment_name: 'homework 5', assignment_id: '7'}];
-​
-
-/*
-WILL BE USED ONCE DATA BASE IS BEING USED
-async function reloadClasses() {
-    try {
-        const data = await readFile(CLASS_FILE, { encoding: 'utf8' });
-        class_records = JSON.parse(data);
-    } catch (err) {
-        class_records = [];
-    }
-}
-
-//Unsure if needed, are we going to add classes or is that going to be static?
-async function saveClasses() {
-    try {
-        const data = JSON.stringify(class_records);
-        await writeFile(CLASS_FILE, data, { encoding: 'utf8' });
-    } catch(err) {
-        class_records = [];
-    }
-}
-*/
 
 app.get('/dashboard', async (request, response) => {
+    console.log("hello from server");
     const options = request.query; 
     const user = options.user;
-    const classesArr = user_records.filter(item => item.user_id === user)[0].inClasses;
-    let userClasses = class_records.filter(item => classesArr.includes(item.class_id));
+    let tempUserRecords = user_records;
+    let tempClassRecords = class_records;
+    const classesArr = tempUserRecords.filter(item => item.user_id === user)[0].inClasses;
+    console.log(classesArr);
+    let userClasses = class_records;
     console.log(userClasses);
     response.send(userClasses);
 })
@@ -75,7 +57,7 @@ app.get('/dueToday', async (request, response) =>{
 app.all('*', async (request, response) => {
     response.status(404).send(`Not found: ${request.path}`)
 });
-​
+
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
 });
