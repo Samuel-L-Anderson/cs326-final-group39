afaea9861132e9376b78ef213b34e1c3975e4768
willhogan370 <90738066+willhogan370@users.noreply.github.com>
2022-04-20 22:35:01 -0400

    Updating server to be compatible with dashboard

--- server.js
+++ server.js
@@ -17,14 +17,44 @@ const USER_FILE = 'user_records.json'
 //CLASSES ORDERED BY ID 1-4 etc
 //CHANNELS ORDERED BY ID 1.1-1.6 etc
 
-let class_records = [{ class: 'cs326', studentcount: '187', class_id: '1', professor: 'Tim Richards' },
-{ class: 'cs446', studentcount: '146', class_id: '2', professor: '' }, { class: 'cs365', studentcount: '90', class_id: '3', professor: '' },
-{ class: 'econ103', studentcount: '250', class_id: '4', professor: '' }];
+let class_records = [{ class: 'cs326', studentcount: '187', class_id: '1' , professor: 'Tim Richards'},
+{ class: 'cs446', studentcount: '146', class_id: '2', professor: "Tom Brady"}, { class: 'cs365', studentcount: '90', id: '3', professor: "Bobby Oar"},
+{ class: 'econ103', studentcount: '250', id: '4', professor: "Mac Jones"}];
 
 let user_records = [{ name: 'Joe', instructor: 'False', user_id: '1', inClasses: [1, 2, 3, 4] },
 { name: 'Jeff', instructor: "False", user_id: '2', inClasses: [1, 2, 3, 4] }];
 
+let assignment_records = [{class_id: '3', day: '20', month: '3', year: '2022', assignment_name: 'homework 5', assignment_id: '7'}];
+
 let message_records = [];
+
+app.get('/dashboard', async (request, response) => {
+    console.log("hello from server");
+    const options = request.query; 
+    const user = options.user;
+    let tempUserRecords = user_records;
+    let tempClassRecords = class_records;
+    const classesArr = tempUserRecords.filter(item => item.user_id === user)[0].inClasses;
+    console.log(classesArr);
+    let userClasses = class_records;
+    console.log(userClasses);
+    response.send(userClasses);
+})
+
+app.get('/dueToday', async (request, response) =>{
+    const options = request.query; 
+    const class_id = options.id;
+    const current = new Date();
+    let dueToday = assignment_records.filter(item => (
+        class_id === item.class_id &&
+        item.day === current.getDay() &&
+        item.month === current.getMonth() &&
+        item.year === current.getFullYear() 
+        ));
+    const returnVal = (dueToday.length > 0) ? dueToday[0].assignment_name : "N/A";
+    console.log(returnVal);
+    response.send(returnVal); 
+});
 
 async function reloadMessages() {
     try {
