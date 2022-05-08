c3436f261e2468e305f0637e958c204fd097f212
willhogan370 <90738066+willhogan370@users.noreply.github.com>
2022-05-07 22:56:45 -0400

    overwriting main

--- client/Login/crud_login.js
+++
@@ -1,9 +0,0 @@
-export async function getUser(email,password,spireId) {
-    console.log(email);
-    console.log(password);
-    console.log(spireId);
-    const response = await fetch(`/login?email=${email}&password=${password}&spireId=${spireId}`, {
-    method: 'GET'});
-    const data = await response.json();
-    return data;
-}
